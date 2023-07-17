adbc9971bcfccb5a1b04e51c52522a9e58c4a1b0
xuehui <wei.xuehui@trs.com.cn>
2023-07-17 12:28:10 +0800

    初始化 ocr审批

--- src/views/aiApproval/index.vue
+++ src/views/aiApproval/index.vue
@@ -1,3 +1,88 @@
 <template>
-  <div>1</div>
+  <div class="container">
+    <div class="tools"></div>
+    <div class="content">
+      <div class="content-header">
+        <span class="content-title">
+          <i class="iconfont icon-shenpiyemiantubiao"></i>审查项目名称显审查项目名称显示审查项目名称显示</span>
+        <span class="content-btns">
+          <el-button>返回</el-button>
+          <el-button type="primary">提交</el-button>
+        </span>
+      </div>
+      <div class="content-cont">
+        <file-preview></file-preview>
+        <orcTxt></orcTxt>
+        <editorial></editorial>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import filePreview from './components/file-preview'
+import orcTxt from './components/ocr-txt'
+import editorial from './components/editorial'
+export default {
+  name: 'aiApproval',
+  components: { filePreview, orcTxt, editorial },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.container {
+  display: flex;
+  height: 100%;
+
+  .tools {
+    background: #ffffff;
+    width: 60px;
+  }
+
+  .content {
+    flex: 1;
+    display: flex;
+    padding: 16px 24px;
+    flex-direction: column;
+  }
+}
+
+.content-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
+  .el-button {
+    line-height: 22px;
+    padding: 6px 16px;
+    border: none;
+  }
+}
+.content-title{
+  font-size: 16px;
+  font-weight: 700;
+
+  i{
+    color: #EB5D78;
+    margin-right: 12px;
+  }
+}
+.content-btns{
+  margin-left: 20px;
+  white-space: pre;
+}
+.content-cont{
+  flex: 1;
+  display: flex;
+  gap: 12px;
+  >div{
+    background: #ffffff;
+    flex: 1;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px 0px #4343430D;
+  }
+}
+</style>
