ca27f065e6e053c8a5306cea278ccdf1f5f2633a
wzp <w1749298515@163.com>
2023-08-02 10:47:32 +0800

    bug修复02

---
+++ src/views/front/show-review.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="show-review">
+    <file-preview :url="url"></file-preview>
+  </div>
+</template>
+
+<script>
+import FilePreview from '@/components/filePreview';
+export default {
+  props: {},
+  data() {
+    return {
+      url: this.$route.query.url
+    };
+  },
+  created() {
+    console.log(this.$route.query.url);
+  },
+  components: {
+    FilePreview
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.show-review {
+  width: 100%;
+  height: 100%;
+}
+</style>
