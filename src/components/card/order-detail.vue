d1893ac134b1879d0bdc60df6384795e100c5e26
nimeimix <huo.linchun@trs.com.cn>
2023-08-03 12:07:14 +0800

    调整详情页的component

--- src/components/card/order-detail.vue
+++ src/components/card/order-detail.vue
@@ -256,7 +256,7 @@ export default {
       // 草稿
       if (item.taskStatus == 0) {
         this.status = 0;
-        this.crtComp = "approvedOpinionCard";
+        this.crtComp = "approvalRecordCard";
         //后期扩展,审批也分好几种类型
       }
       // 审批中 主要区分是否OCR审批、部门审批  所有的的节点
@@ -269,7 +269,7 @@ export default {
           this.isOCR = Math.random()
         } else {
           this.status = 0;
-          this.crtComp = "approvedOpinionCard";
+          this.crtComp = "approvalRecordCard";
         }
       }
       // 状态待修改 
