26b4dd8485d85ff6c74599a3282aeb59ebb18b17
huoalong <huoal@idss-cn.com>
2023-08-05 15:42:54 +0800

    fix: 表单权限设置

--- src/views/common/process/config/FormAuthorityConfig.vue
+++ src/views/common/process/config/FormAuthorityConfig.vue
@@ -107,7 +107,6 @@ export default {
           })
         }
       })
-      console.log(this.formData)
     },
     handleCheckAllChange(){
 
@@ -120,7 +119,7 @@ export default {
       deep: true,
       handler(){
         const set = new Set(this.formPerms.map(f => f.perm))
-        this.permSelect = set.size === 1 ? set.values()[0] : ''
+        this.permSelect = set.size === 1 ? this.formPerms[0].perm : ''
       }
     }
   }
