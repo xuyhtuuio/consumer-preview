68bd9fed8ea22ebd78e63315e202c47969be2e02
wzp <w1749298515@163.com>
2023-08-05 15:24:30 +0800

    意见管理接口

--- src/api/admin-opinion.js
+++ src/api/admin-opinion.js
@@ -1,12 +1,21 @@
-
-import request from '@/api/request.js'
+import qs from 'qs';
+import request from '@/api/request.js';
 // 获取列表
-export function getPageList(data) {
+export function getPageList(params) {
   return request({
     url: '/cpr/opinion/list',
     method: 'get',
-    data
-  }).then(({data:{data:res}}) => {
-    return res
-  })
+    params,
+  }).then(({ data: { data: res } }) => {
+    return res;
+  });
+}
+export function getSearchList(params) {
+  return request({
+    url: '/cpr/keyword/search',
+    method: 'get',
+    params,
+  }).then(({ data: { data: res } }) => {
+    return res;
+  });
 }
