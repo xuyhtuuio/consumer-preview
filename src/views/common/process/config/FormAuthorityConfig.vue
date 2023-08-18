7ff6ee85b156eaaf9ca74b5ae7039254080e9d55
huoalong <huoal@idss-cn.com>
2023-08-18 11:27:38 +0800

    feat(表单管理)：新增单选项组

--- src/views/common/process/config/FormAuthorityConfig.vue
+++ src/views/common/process/config/FormAuthorityConfig.vue
@@ -97,6 +97,7 @@ export default {
         for (let i in perms) {
           if (perms[i].id === form.id){
             perms[i].title = form.title
+            perms[i].module = form.module
             isLoad = true
             break;
           }
@@ -104,10 +105,15 @@ export default {
         if (!isLoad){
           const isRoot = this.$store.state.selectedNode.type === 'ROOT'
           const secondCheck = this.$store.state.selectedNode.type === 'APPROVAL-TWO'
+          let perm = (isRoot || secondCheck) ? 'E':'R'
+          if (form.module === '审批人填写') {
+            perm = 'H'
+          }
           perms.push({
             id: form.id,
             title: form.title,
-            perm: (isRoot || secondCheck) ? 'E':'R'
+            module: form.module,
+            perm
           })
         }
       })
