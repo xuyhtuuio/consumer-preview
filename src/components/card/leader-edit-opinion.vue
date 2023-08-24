ae65a241018b0a9863b72dba77cdff33009b9f4d
nimeimix <huo.linchun@trs.com.cn>
2023-08-24 18:57:52 +0800

    驳回

--- src/components/card/leader-edit-opinion.vue
+++ src/components/card/leader-edit-opinion.vue
@@ -8,10 +8,10 @@
         </el-radio-group>
       </el-form-item>
       <!-- &&refuseWay=='TO_BEFORE' -->
-      <el-form-item label="请选择驳回人" prop="disavower" v-if="form.isAccept == '0' && refuseWay == 'TO_BEFORE'">
-        <el-select v-model="form.disavower" placeholder="请选择驳回节点/驳回人" @change="updateForm">
-          <el-option v-for="item in disavower" :key="item.id"
-            :label="item.name + '/' + item.label + ' 【' + item.nodeName + '】'" :value="item.id">
+      <el-form-item label="请选择驳回人" prop="crtDisavower" v-if="form.isAccept == '0'&&refuseWay=='TO_BEFORE'">
+        <el-select v-model="form.crtDisavower" placeholder="请选择驳回节点/驳回人" @change="updateForm('disavower')">
+          <el-option v-for="(item, index) in disavower" :key="index"
+            :label="item.name + '/' + item.label + ' 【' + item.nodeName + '】'" :value="item">
             <span style="
              width: 33%;
                 font-weight: 700;
@@ -48,8 +48,8 @@
         </el-select>
       </el-form-item>
       <!--  && assignedType == 'SELF_SELECT' -->
-      <el-form-item label="请选择审批人" prop="approver" v-if="form.isAccept == '1' && assignedType == 'SELF_SELECT'">
-        <el-select v-model="form.approver" placeholder="需【下一节点名称】审批，请选择审批人" @change="updateForm">
+      <el-form-item label="请选择审批人" prop="crtApprover" v-if="form.isAccept == '1' && assignedType == 'SELF_SELECT'">
+        <el-select v-model="form.crtApprover" placeholder="需【下一节点名称】审批，请选择审批人" @change="updateForm">
           <el-option v-for="item in approver" :key="item.id"
             :label="item.name + '/' + item.label + ' 【' + item.nodeName + '】'" :value="item.id">
             <p class="flex">
@@ -95,7 +95,7 @@
 </template>
 <script>
 export default {
-  name: "leader-edit-opinion",
+
   props: {
   },
   data() {
@@ -104,10 +104,10 @@ export default {
       approver: [],
       rules: {
         isAccept: [{ required: true, message: "请选择", trigger: ["blur"] }],
-        approver: [
+        crtApprover: [
           { required: true, message: "请选择审批人", trigger: ["blur"] },
         ],
-        disavower: [
+        crtDisavower: [
           { required: true, message: "请选择驳回节点/驳回人", trigger: ["blur"] },
         ],
         reason: [
@@ -120,24 +120,33 @@ export default {
         approver: "",
         content: "",
         reason: '',
-        disavower: '',
+        crtDisavower: null,
       },
       refuseWay: '',  // TO_NODE 驳回到指定节点 / TO_BEFORE 审批人自选（前序节点）
       assignedType: '', // SELF_SELECT 流程配置中下一节点审批人设置时选择“上一审批人选择”，增加选择审批人选择则框
+      externalData:{},// 
     };
   },
   watch: {},
   methods: {
     initData(data) {
+      this.externalData = data
       this.disavower = data.disavower
       this.approver = data.approver
       this.refuseWay = data.refuseWay
       this.assignedType = data.assignedType
     },
-    updateForm() {
-      const params = {
+    updateForm(flag) {
+      let params = {
+        ...this.externalData,
         ...this.form,
-        assignedType: this.assignedType
+        assignedType: this.assignedType,
+      }
+      if(this.form.isAccept=='0'){
+        params={
+          ...params,
+          ...this.form.crtDisavower
+        }
       }
       this.$refs["form"].validate((valid) => {
         this.$store.commit('setEditOpinionRequired', valid)
