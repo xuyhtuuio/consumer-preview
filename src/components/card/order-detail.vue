bbb77471b976c4cecca809c30218fbd83a9f5a28
nimeimix <huo.linchun@trs.com.cn>
2023-09-14 16:57:09 +0800

    修复缺陷

--- src/components/card/order-detail.vue
+++ src/components/card/order-detail.vue
@@ -2,7 +2,7 @@
  * @Author: nimeimix huo.linchun@trs.com.cn
  * @Date: 2023-08-29 13:49:23
  * @LastEditors: nimeimix huo.linchun@trs.com.cn
- * @LastEditTime: 2023-09-14 16:53:35
+ * @LastEditTime: 2023-09-14 16:56:56
  * @FilePath: /consumer-preview/src/components/card/order-detail.vue
  * @Description: 左侧：工单详细信息   右侧：工单处于不同状态下，会回显不同的信息
 -->
@@ -431,9 +431,9 @@ export default {
       return
     }
     this.clearStoreStatus()
+    this.judgeStatus()
   },
   created() {
-    this.judgeStatus()
   },
   methods: {
     /**
