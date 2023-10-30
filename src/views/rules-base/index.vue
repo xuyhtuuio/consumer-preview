5d2b8673b774658c52a43ee59c4caf9ea1facb55
nimeimix <huo.linchun@trs.com.cn>
2023-10-30 18:25:14 +0800

    main_right不要overflow auto

--- src/views/rules-base/index.vue
+++ src/views/rules-base/index.vue
@@ -2,7 +2,7 @@
  * @Author: nimeimix huo.linchun@trs.com.cn
  * @Date: 2023-10-23 21:17:11
  * @LastEditors: nimeimix huo.linchun@trs.com.cn
- * @LastEditTime: 2023-10-30 15:01:50
+ * @LastEditTime: 2023-10-30 17:52:50
  * @FilePath: /consumer-preview/src/views/rules-base/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -209,7 +209,8 @@ export default {
     border-radius: 10px;
     background: #fff;
     margin-right: 8px;
-    height: calc(100% - 58px);
+    // height: calc(100% - 58px);
+    height: fit-content;
     li {
       padding: 8px 12px;
       margin-bottom: 8px;
