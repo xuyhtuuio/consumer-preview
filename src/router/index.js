b9f8045ea5ce03d50d08d14a77d99c9a71225be1
cuiyunhao <cuiyunhao@123.com>
2023-11-06 16:18:20 +0800

    feat：latest

--- src/router/index.js
+++ src/router/index.js
@@ -133,7 +133,7 @@ const router = new Router({
       path: '/recheck',
       name: 'recheck',
       component: () => import('@/views/intelligent-recheck/home'),
-      meta: { title: '智能回检', viewport },
+      meta: { code: ['recheckIndex'], title: '智能回检', viewport },
       redirect: '/recheck/recheck-index',
       children: [
         {
